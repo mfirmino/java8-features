9a7c865e0cba93105ee15f40b97be3d37abc8eaf
Marcelo Bonato Firmino <mfirmino@gmail.com>
2016-06-06 23:09:46 -0300

    Initial commit

---
+++ src/br/com/estudo/java8/OrdenaStrings.java
@@ -0,0 +1,16 @@
+package br.com.estudo.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdenaStrings {
+
+    public static void main(String[] args) {
+        List<String> palavras = new ArrayList<String>();
+        palavras.add("Alura Online");
+        palavras.add("editora");
+        palavras.add("bartolomeu");
+        palavras.add("Intellij");
+
+    }
+}
